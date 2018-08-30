033b678176cc9da1547a90a21d2b7203f99e5187
Yusuf Irwandi <yusuf.chapterzero@gmail.com>
2018-08-30 16:40:29 +0700

    Auto ssh agent

---
+++ ssh/.auto_sshagent.sh
@@ -0,0 +1,17 @@
+SSH_ENV="$HOME/.ssh/env"
+
+function start_agent {
+    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+    chmod 600 "${SSH_ENV}"
+    . "${SSH_ENV}" > /dev/null
+    /usr/bin/ssh-add > /dev/null 2>&1;
+}
+
+if [ -f "${SSH_ENV}" ]; then
+    . "${SSH_ENV}" > /dev/null
+    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+        start_agent;
+    }
+else
+    start_agent;
+fi
